a9b20db8e2229fdb788a7993ffea321c5c14575e
RedBull-Monster-HOT6 <pjjpjj111@naver.com>
2025-04-09 20:00:35 +0900

    5점 만들고 틀린부분 주석으로 표시해둠

--- C++_univ_vs2022/실습42번/word_manager.cpp
+++ C++_univ_vs2022/실습42번/word_manager.cpp
@@ -27,17 +27,20 @@ void updateWord(map<string, size_t>& words, const string& rawWord, size_t value)
         find->second = static_cast<size_t>(value);
         cout << "Updated: " << find->first << " " << find->second << endl;
     }
+    else {
+        cout << "Error: '" << change2low(tmp) << "' not found. Cannot update." << endl;//그리고 이렇게 예외 처리 안해서 계속 틀렸던 것이었음 왜 그랬나 싶었다. 이렇게 예외처리 문제 잘 읽고 꼭 써라
+    }
 }
 
 void deleteWord(map<string, size_t>& words, const string& rawWord) {
     string tmp = rawWord;
     auto find = words.find(change2low(tmp));
-    if (find == words.end()) {
-        cout << "Error: '" << change2low(tmp) << "' not found. Cannot delete." << endl;
-    }
-    else if (find != words.end()) {
-        words.erase(find);
+    if (find != words.end()) {
         cout << "Deleted: " << find->first << endl;
+        words.erase(find); //cout << "Deleted: " << find->first << endl; 이걸 먼저 출력한 후에 words.erase(find);을 해야 한다
+    }
+    else {
+        cout << "Error: '" << change2low(tmp) << "' not found. Cannot delete." << endl;
     }
 }
 
