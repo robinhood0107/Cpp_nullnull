0d4411f013c0b9ecf5027fc311d9f0e0486b4c29
YourUsername <pjjpjj111@naver.com>
2025-04-16 21:51:00 +0900

    현재 2.22점... 심각한 오류 존재

--- C++_univ_vs2022/실습52번/inventory.cpp
+++ C++_univ_vs2022/실습52번/inventory.cpp
@@ -14,4 +14,78 @@ namespace {
 }
 
 // Implement your code
+void create_item(const std::string& name, double price, int qty) {
+    Item item;
+    int len = generate_next_id();
+    item.id = len;
+    item.name = name;
+    item.price = price;
+    item.qty = qty;
+    data.push_back(item);
+}
+const inventory get_inventory() {
+    //원래는 const Item& a, const Item& b가 인자로들어가는데 auto 쓰면 된다.
+    //a>b 이게 오름차순인거 기억.
+    std::sort(data.begin(), data.end(), [](auto a, auto b) {return a.price < b.price; });
+    return data;
+}
 
+bool find_item(int id, Item& found_item) {
+    auto it = std::find_if(data.begin(), data.end(), [&id](auto x) {return x.id == id; });
+    if (it != data.end()) {
+        //못찾으면 end반환 꼭 기억
+        found_item.id = it->id;
+        found_item.price = it->price;
+        found_item.qty = it->qty;
+        return true;
+    }
+    else {
+        return false;
+    }
+
+
+}
+
+bool update_item(int id, double new_price, int new_qty) {
+    auto it = std::find_if(data.begin(), data.end(), [&id](auto x) {return x.id == id; });
+    if (it != data.end()) {
+        it->price = new_price;
+        it->qty = new_qty;
+        return true;
+    }
+    else
+        return false;
+}
+
+bool delete_item(int id) {
+    if (std::find_if(data.begin(), data.end(), [&id](auto x) {return x.id == id; }) != data.end()) {
+        std::remove_if(data.begin(), data.end(), [&id](auto x) {return x.id == id; });
+        return true;
+    }
+    else
+        return false;
+}
+
+
+int generate_next_id() {
+    int id = 0;
+    auto it = std::find_if(data.begin(), data.end(), [&id](auto x) {return x.id == id; });
+    int len = data.size();
+    if (it == data.end()) {
+        return 1;
+    }
+    else {
+        return len + 1;
+    }
+
+}
+
+double calculate_total_value() {
+    double sum = 0;
+    std::vector<double> vec = {};
+    for (auto& it : data) {
+        vec.push_back(it.price * it.qty);
+    }
+    sum = accumulate(vec.begin(), vec.end(), 0);
+    return sum;
+}
