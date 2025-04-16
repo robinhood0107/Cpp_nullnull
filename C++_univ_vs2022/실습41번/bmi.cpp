1af94f473776da6376cdc6ea9b04ad9f8d80cf87
YourUsername <pjjpjj111@naver.com>
2025-04-16 21:50:01 +0900

    41번 풀이

--- C++_univ_vs2022/실습41번/bmi.cpp
+++ C++_univ_vs2022/실습41번/bmi.cpp
@@ -4,15 +4,33 @@
 #include <iostream>
 #include <algorithm>
 #include <iomanip>
+using namespace std;
 
 bool addBMI(std::vector<int>& bmis, int weight, int height) {
-    
+    if (weight < 0 || height < 0) {
+        return false;
+    }
+    else {
+        int bmi = static_cast<int>(100.0 * weight * 10000 / (height * height));
+        bmis.push_back(bmi);
+        return true;
+    }
 }
 
 bool displayBMIs(const std::vector<int>& bmis) {
-    
+    vector<int> vec = bmis;
+    sort(vec.begin(), vec.end());
+    cout << vec.size() << "\n";
+    for (const auto& lt : vec) {
+        double bmi = static_cast<double>(lt) / 100.0;
+        std::cout << std::fixed << std::setprecision(2) << bmi << " ";
+    }
+    cout << endl;
+    return true;
 }
 
 bool deleteByValue(std::vector<int>& bmis, int value) {
-    
+    auto size = bmis.size();
+    bmis.erase(remove(bmis.begin(), bmis.end(), value), bmis.end());
+    return bmis.size() < size;
 }
